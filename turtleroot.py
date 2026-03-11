da89b1af6bebdc01cdbb09100bb9901f493b8f2c
Chucny <166989589+Chucny@users.noreply.github.com>
2026-03-11 13:53:30 +0200

    Update turtleroot.py

--- turtleroot.py
+++ turtleroot.py
@@ -3,7 +3,36 @@ import gzip
 import shutil
 import tempfile
 import struct
-print("TurtleRoot - Made by Chucny")
+print("TurtleRoot - Made by Chucny                    v 1.1")
+def prRed(s): print("\033[91m {}\033[00m".format(s))
+def prGreen(s): print("\033[92m {}\033[00m".format(s))
+def prYellow(s): print("\033[93m {}\033[00m".format(s))
+def prLightPurple(s): print("\033[94m {}\033[00m".format(s))
+def prPurple(s): print("\033[95m {}\033[00m".format(s))
+def prCyan(s): print("\033[96m {}\033[00m".format(s))
+def prLightGray(s): print("\033[97m {}\033[00m".format(s))
+def prBlack(s): print("\033[90m {}\033[00m".format(s))
+prGreen("""                                                                
+                         ========                               
+                       ==+**++**+==                             
+                       ==+**++**+==                             
+                       ============                             
+                       ============                             
+                        ==========                              
+                       ============                             
+             ================================                   
+           ============+#%%#+==+##++%%+========                 
+            =========+##*+++===+%%++%%+=======                  
+              ========+*#%%#+==+%@%%@@#=====                    
+             ==========++***#*+++*****+======                   
+           ============+*###**+================                 
+           ============+*##*+==================                 
+             ================================                   
+                       ============                             
+                         ========                               
+                           ====                                 
+                           ====                                 
+""")
 def turtleRoot(input_img, su_src):
     output_img = input_img.replace(".img", "_su.img")
     input_img = os.path.abspath(input_img)
